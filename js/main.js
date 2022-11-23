a2a016f3d0519be2d21bf524063d1ce4c5890476
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-11-23 19:04:58 +0100

    completed first milestone

--- js/main.js
+++ js/main.js
@@ -3,10 +3,31 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            toDoList: [
+                {
+                    text: "Controllare offerte black friday",
+                    done: true,
+                },
+                {
+                    text: "Controllare se sono arrivati soldi sul conto ",
+                    done: false,
+                },
+                {
+                    text: "Chiamare zio Pino",
+                    done: true,
+                }, 
+                {
+                    text: "Portare Jerry a fare la pipì",
+                    done: true,
+                },
+                {
+                    text: "Comprare guida per studiare HTML",
+                    done: false,
+                },
+            ]
         }
     },
     methods: {
-        
+
     }
 }).mount('#app')
